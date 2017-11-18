75bdcbba59ff24cb68beb87612de8714af8620e3
Nelson Andres Castillo Saza <nelson.castillo@rappi.com>
2017-11-18 13:15:32 -0500

    Added notes in the solution of LIS problem

--- src/com/yiyo/study/dynamic_programming/LongestIncreasingSubsequence.java
+++ src/com/yiyo/study/dynamic_programming/LongestIncreasingSubsequence.java
@@ -20,22 +20,30 @@ public class LongestIncreasingSubsequence {
     }
 
     private static int lengthOfLIS(int[] nums) {
+        // Corner case
         if (nums.length == 0) {
             return 0;
         }
 
         int[] result = new int[nums.length];
+        // Every value has a LIS combination of length 1 with itself
         Arrays.fill(result, 1);
+        // We need to store the maximum of all combination because the array is not sorted
+        // so the last value of the array is not always the greatest
         int totalMax = 1;
 
         for (int i = 1; i < nums.length; i++) {
             int max = 1;
+            // We have to check every value from 0 to i looking for possible combinations
             for (int j = 0; j < i; j++) {
+                // If some previous value is less than the current then we add 1 to our current position
+                // and store the maximum between that value and our local max
                 if (nums[j] < nums[i]) {
                     max = Math.max(max, 1 + result[j]);
                 }
             }
             result[i] = max;
+            // Here we make sure we only keep our greatest maximum for all the array and not just for the current num
             totalMax = Math.max(max, totalMax);
         }
         return totalMax;
