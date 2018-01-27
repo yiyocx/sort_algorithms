cb544fa038e5289a701be45bbd4a41e7eac88492
Nelson Andres Castillo Saza <nelson.castillo@rappi.com>
2018-01-27 17:11:58 -0500

    Added Reverse Linked List problem

---
+++ src/com/yiyo/study/linked_lists/ReverseLinkedList.java
@@ -0,0 +1,42 @@
+package com.yiyo.study.linked_lists;
+
+public class ReverseLinkedList {
+
+    /*
+    Reverse a linked list. Do it in-place and in one-pass.
+
+    For example:
+    Given 1->2->3->4->5->NULL,
+    return 5->4->3->2->1->NULL.
+     */
+    public static void main(String[] args) {
+        ReverseLinkedList linkedList = new ReverseLinkedList();
+
+        ListNode A = new ListNode(1);
+        A.next = new ListNode(2);
+        A.next.next = new ListNode(3);
+        A.next.next.next = new ListNode(4);
+        A.next.next.next.next = new ListNode(5);
+
+        ListNode reversedList = linkedList.reverseList(A);
+        while (reversedList != null) {
+            System.out.println(reversedList.val);
+            reversedList = reversedList.next;
+        }
+    }
+
+    private ListNode reverseList(ListNode A) {
+        ListNode prev = null;
+        ListNode next;
+        ListNode current = A;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        A = prev;
+        return A;
+    }
+}
